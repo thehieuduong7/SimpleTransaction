bdd171032374071cd5f559aa40b142da0134b594
thehieuduong7 <hieuduon959@gmail.com>
2022-08-05 17:14:33 +0700

    foreign key

--- main.go
+++ main.go
@@ -7,29 +7,30 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func Init() *gorm.DB {
 	dbURL := "postgres://postgres:123456@localhost:5433/demo1"
 
-	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{
+		Logger:                                   logger.Default.LogMode(logger.Info), // Log câu lệnh sql trong console
+		DisableForeignKeyConstraintWhenMigrating: false,
+	})
 
 	if err != nil {
 		fmt.Print("loi")
 	}
 
-	db.AutoMigrate(&models.Account{}, &models.User{}, &models.Transaction{})
+	db.Migrator().CreateTable(&models.User{}, &models.Account{}, &models.Transaction{})
 	return db
 }
 func main() {
-	fmt.Print(Init())
-	// Init()
-
+	Init()
 	// user := models.User{Name: "Jinzhu", Phone_number: "123423", Email: "19923"}
-
 	// db.Create(&user) // pass pointer of data to Create
-
 	// fmt.Print(user.User_id)
+	// fmt.Print(user.Accounts)
 
 	// account := models.Account{
 	// 	User_id: 2, Surplus: -4,
--- model/models.go
+++ model/models.go
@@ -5,27 +5,30 @@ import (
 )
 
 type User struct {
-	User_id      int    `json:"user_id" gorm:"primaryKey;autoIncrement"`
-	Name         string `json:"name gorm:NOT NULL"`
-	Phone_number string `json:"phone_number"`
-	Email        string `json:"email"`
+	User_id      int       `json:"user_id"  gorm:"primaryKey;autoIncrement;unique"`
+	Name         string    `json:"name" gorm:"NOT NULL"`
+	Phone_number string    `json:"phone_number"`
+	Email        string    `json:"email"`
+	Accounts     []Account `gorm:"foreignKey:User_id;references:User_id;constraint:OnUpdate:CASCADE"`
 }
 
 type Account struct {
-	Account_number int `json:"account_id" gorm:"primaryKey;autoIncrement"`
-	User_id        int
-	Surplus        float64   `json:"surplus" gorm:" check:surplus >= 0"`
-	Create_at      time.Time `json:"create_at" gorm:"autoCreateTime:true"`
-	Is_active      bool      `json:"is_active" gorm:"type:bool;default:true"`
+	Account_number           int           `json:"account_no" gorm:"primaryKey;autoIncrement;unique"`
+	User_id                  int           `gorm:"NOT NULL"`
+	Surplus                  float64       `json:"surplus" gorm:" check:surplus >= 0"`
+	Create_at                time.Time     `json:"create_at" gorm:"autoCreateTime:true"`
+	Is_active                bool          `json:"is_active" gorm:"type:bool;default:true"`
+	Transaction_resources    []Transaction `gorm:"foreignKey:Account_no_rsc;references:Account_number;constraint:OnUpdate:CASCADE"`
+	Transaction_destinations []Transaction `gorm:"foreignKey:Account_no_des;references:Account_number;constraint:OnUpdate:CASCADE"`
 }
 
 type Transaction struct {
-	Transaction_id int `json:"transaction_id" gorm:"primaryKey;autoIncrement"`
-	Account_no_rsc int
-	Account_no_des int
+	Transaction_id int       `json:"transaction_id" gorm:"primaryKey;autoIncrement"`
+	Account_no_rsc int       `json:"acc_no_rsc" gorm:"NOT NULL"`
+	Account_no_des int       `json:"acc_no_des" gorm:"NOT NULL"`
 	Message        string    `json:"message" gorm:"size:500"`
 	Amount         float64   `json:"amount" gorm:" check:amount >= 0; NOT NULL"`
 	Date_start     time.Time `json:"date_start" `
 	Date_success   time.Time `json:"date_success"`
-	Is_success     bool
+	Is_active      bool      `json:"is_active" gorm:"type:bool;default:true"`
 }

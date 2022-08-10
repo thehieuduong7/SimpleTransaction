76341ed499cfe977e44c7e3552be73be7c23ceaf
xuantruong <truinghhh@gmail.com>
2022-08-10 15:23:07 +0700

    add accountController file add accountRepository file add unit_test file add constraint file

---
+++ controller/AccountController.go
@@ -0,0 +1,41 @@
+package controller
+
+import (
+	models "internBE.com/model"
+	"internBE.com/respository"
+)
+
+type AccountController interface {
+	CreatAccount(account *models.Account) *models.Account
+	UpdateAccount(account *models.Account) models.Account
+	DeleteAccount(id int)
+	GetAccountByUserId(id int) []models.Account
+	GetAccountById(id int) models.Account
+}
+type accountController struct {
+	AccountRepo respository.AccountRepository
+}
+
+func NewAccountController(repo respository.AccountRepository) AccountController {
+	return &accountController{AccountRepo: repo}
+}
+
+func (controller *accountController) CreatAccount(account *models.Account) *models.Account {
+	return controller.AccountRepo.CreatAccount(account)
+}
+
+func (controller *accountController) UpdateAccount(account *models.Account) models.Account {
+	return controller.AccountRepo.UpdateAccount(account)
+}
+
+func (controller *accountController) DeleteAccount(id int) {
+	controller.AccountRepo.DeleteAccount(id)
+}
+
+func (controller *accountController) GetAccountByUserId(id int) []models.Account {
+	return controller.AccountRepo.GetAccountByUserId(id)
+}
+
+func (controller *accountController) GetAccountById(id int) models.Account {
+	return controller.AccountRepo.GetAccountById(id)
+}

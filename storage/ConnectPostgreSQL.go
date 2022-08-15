43fcd72f54ce291dbefdd4b22e3541c3fbf4ee30
dabadaxx <anhtramvu97@gmail.com>
2022-08-15 14:02:10 +0700

    update constantGlobal

--- storage/ConnectPostgreSQL.go
+++ storage/ConnectPostgreSQL.go
@@ -3,43 +3,33 @@ package storage
 import (
 	"fmt"
 	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"internBE.com/constantGlobal"
 	"internBE.com/entity"
 )
-
-//type Config struct {
-//	Host     string
-//	Port     string
-//	Password string
-//	User     string
-//	DBName   string
-//}
 
 var DB *gorm.DB
 
 func Connect() *gorm.DB {
-	errEnv := godotenv.Load()
-	if errEnv != nil {
-		panic("Failed to load env file")
-	}
+	// errEnv := godotenv.Load()
+	// if errEnv != nil {
+	// 	panic("Failed to load env file")
+	// }
 
-	Host := os.Getenv("DBHost")
-	Port := os.Getenv("DBPort")
-	User := os.Getenv("DBUser")
-	Password := os.Getenv("DBPassword")
-	DBName := os.Getenv("DBName")
+	// Host := os.Getenv("DBHost")
+	// Port := os.Getenv("DBPort")
+	// User := os.Getenv("DBUser")
+	// Password := os.Getenv("DBPassword")
+	// DBName := os.Getenv("DBName")
 
 	dsn := fmt.Sprintf("host=%s port=%s user=%s  password=%s dbname=%s sslmode=disable",
-		Host, Port,
-		User,
-		Password,
-		DBName,
+		constantGlobal.Host, constantGlobal.Port,
+		constantGlobal.User,
+		constantGlobal.Password,
+		constantGlobal.DBName,
 	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info), // Log câu lệnh sql trong console
